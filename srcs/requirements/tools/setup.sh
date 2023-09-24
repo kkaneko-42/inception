07658e62782d18c74884c5bd01a36506a29ef3aa
kkaneko-window_laptop <gorutiru@gmail.com>
2023-09-24 18:55:32 +0900

    change volume dir

--- srcs/requirements/tools/setup.sh
+++ srcs/requirements/tools/setup.sh
@@ -7,7 +7,7 @@ if grep ${DOMAIN_NAME} /etc/hosts; then
     echo "/etc/hosts is already configured"
 else
     if ! sudo sh -c "echo '127.0.0.1    ${DOMAIN_NAME}' >> /etc/hosts"; then
-        echo "/etc/hosts configuration failed. 'sudo' maybe needed."
+        echo "/etc/hosts configuration failed"
         exit 1
     fi
 fi
@@ -25,4 +25,12 @@ else
     openssl x509 -days 3650 -req -sha256 -signkey ${CERTS_DIR}/server.key \
         < ${CERTS_DIR}/server.csr \
         > ${CERTS_DIR}/server.crt
+fi
+
+if [ -d ${VOLUME_DIR} ]; then
+    echo "volume is already configured"
+else
+    sudo mkdir -p ${VOLUME_DIR}
+    sudo cp -r ${ENV_DIR}/requirements/tools/db ${VOLUME_DIR}
+    sudo cp -r ${ENV_DIR}/requirements/tools/htdocs ${VOLUME_DIR}
 fi
